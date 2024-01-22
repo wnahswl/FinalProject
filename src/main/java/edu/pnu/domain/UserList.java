14e2c0812ca7628490a35372becaba48db4011ea
wnahswl <wnahswl@naver.com>
2024-01-22 09:45:22 +0900

    remove source files

--- src/main/java/edu/pnu/domain/UserList.java
+++
@@ -1,9 +0,0 @@
-package edu.pnu.domain;
-
-import java.util.List;
-
-import lombok.Data;
-@Data
-public class UserList {
-	private List<String> users;
-}
--- src/main/java/edu/pnu/persistence/PredictLogRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.pnu.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.pnu.domain.PredictLogDomain;
-
-public interface PredictLogRepository extends JpaRepository<PredictLogDomain, Long>{
-
-}
